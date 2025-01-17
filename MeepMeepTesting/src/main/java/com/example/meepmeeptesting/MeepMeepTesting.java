ad8cdc4072b19482ec76eac053fc27f6676119e3
pokeshah <89949620+pokeshah@users.noreply.github.com>
2025-01-17 13:37:57 -0800

    hanging2

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,6 +7,7 @@ import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
@@ -16,93 +17,38 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(100, 80, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(100, 60, Math.toRadians(180), Math.toRadians(180), 14.50988120951799)
                 .setDimensions(17.75, 16)
                 .build();
 
-        Pose2d beginPos = new Pose2d(-44, -40, Math.toRadians(90)); //new Pose2d(-10, -62, Math.toRadians(-90));
+        Pose2d beginPos = new Pose2d(11, -62, Math.toRadians(-90)); //new Pose2d(-10, -62, Math.toRadians(-90));
 
+        DriveShim drive = myBot.getDrive();
         myBot.runAction(
                 new SequentialAction(
-                        myBot.getDrive().actionBuilder(beginPos)
-                                .splineToLinearHeading(new Pose2d(-56, -54, Math.toRadians(47)), Math.toRadians(47))
-//                                .lineToY(-31.5)
-//                            .splineToLinearHeading(new Pose2d(-48, -40, Math.toRadians(-90)), Math.toRadians(180))
-//                                .waitSeconds(2)
-//                                .splineToLinearHeading(new Pose2d(-56, -54, Math.toRadians(47)), Math.toRadians(47))
-//                                .waitSeconds(1)
-//                                .splineToLinearHeading(new Pose2d(-58, -38, Math.toRadians(90)), Math.toRadians(90))
-//                                .waitSeconds(2)
-//                                .strafeTo(new Vector2d(-56,-38))
-//                                .splineToLinearHeading(new Pose2d(-56, -54, Math.toRadians(47)), Math.toRadians(47))
-//                                .waitSeconds(1)
-//                                .splineToLinearHeading(new Pose2d(-30, -12, Math.toRadians(0)), Math.toRadians(0))
+                        drive.actionBuilder(beginPos)
+                                .splineToConstantHeading(new Vector2d(11,-32.5), -pi/2)
+                                .build(),
+                drive.actionBuilder(new Pose2d(11, -32.5, Math.toRadians(-90)))
+                        .splineToConstantHeading(new Vector2d(11,-33),0 )
+                                //Drive to First Block
+                                .splineToSplineHeading(new Pose2d(32, -15, pi/2), pi/2)
+                                .splineToConstantHeading(new Vector2d(42, -12), -pi/2)
+//                                // Push Block 1
+                                .splineToConstantHeading(new Vector2d(45, -50), pi/2) // 54.5
+//                                // Go back, Spline Turn, and go to block 2
+//                                        .lineToY(-12)
+                                .splineToConstantHeading(new Vector2d(45, -12), pi/2)
+                                .splineToConstantHeading(new Vector2d(52, -12), -pi/2)
+//                                // Push Block 2
+                                .splineToConstantHeading(new Vector2d(50, -50), pi/2) // 54.5
+//                                // Go back, and go pick up Block 1
+                                .splineToConstantHeading(new Vector2d(35, -52), -pi/2)
+                                .splineToConstantHeading(new Vector2d(35, -60), -pi/2, new TranslationalVelConstraint(30))
                                 .build()
                 )
         );
 
-//                // Swayam custom code
-//                .lineToY(-34)
-//                .strafeTo(new Vector2d(9, -36))
-//                // Drive to First Block
-//                .splineToLinearHeading(new Pose2d(34, -27, pi/2), pi/2)
-//                .strafeTo(new Vector2d(39, -12))
-//                // Push Block 1
-//                .splineToLinearHeading(new Pose2d(45, -52, pi/2), pi/2)
-//                // Go back, Spline Turn, and go to block 2
-//                .strafeTo(new Vector2d(45, -48))
-//                .splineToConstantHeading(new Vector2d(54, -12), 0)
-//                // Push Block 2
-//                .strafeTo(new Vector2d(54, -52))
-////                 Go back, and go pick up Block 1
-//                .strafeTo(new Vector2d(34, -52))
-//                .strafeTo(new Vector2d(34, -61.5), new TranslationalVelConstraint(50))
-//                .lineToY(-57)
-//                .splineToLinearHeading(new Pose2d(9, -34, -pi/2), pi/2)
-//                .waitSeconds(0)
-//                .splineToLinearHeading(new Pose2d(34, -50, pi/2), pi/2)
-//                .lineToY(-61.5, new TranslationalVelConstraint(30))
-//                .lineToY(-57)
-//                .splineToLinearHeading(new Pose2d(9, -34, -pi/2), pi/2)
-//                .waitSeconds(0)
-//                .splineToLinearHeading(new Pose2d(34, -50, pi/2), pi/2)
-//                .lineToY(-61.5, new TranslationalVelConstraint(30))
-//
-
-
-                // Rishabh Code
-
-//                .lineToY(-34)
-//                // Drive to First Block
-//                .strafeTo(new Vector2d(34, -34))
-//                .strafeTo(new Vector2d(34,-8.75))
-//                .strafeTo(new Vector2d(45, -8.75))
-////                .splineToConstantHeading(new Vector2d(45, -8.75), pi/2)
-//                // Push Block 1
-//                .strafeTo(new Vector2d(45, -54.5))
-//                // Go back, Spline Turn, and go to block 2
-//                .strafeTo(new Vector2d(45, -48))
-//                .splineToLinearHeading(new Pose2d(45, -8.75, pi/2), pi/2)
-//                .strafeTo(new Vector2d(54, -8.75))
-//                // Push Block 2
-//                .strafeTo(new Vector2d(54, -54.5))
-//                // Go back, and go pick up Block 1
-//                .strafeTo(new Vector2d(34, -40))
-//                .strafeTo(new Vector2d(34, -61.5), new TranslationalVelConstraint(50))
-//
-//                .lineToY(-55)
-//                .splineToLinearHeading(new Pose2d(9, -34, -pi/2), pi/2)
-//                .waitSeconds(0)
-//                .splineToLinearHeading(new Pose2d(34, -50, pi/2), pi/2)
-//                .lineToY(-61.5, new TranslationalVelConstraint(30))
-//                .lineToY(-55)
-//                .splineToLinearHeading(new Pose2d(9, -34, -pi/2), pi/2)
-//                .waitSeconds(0)
-//                .splineToLinearHeading(new Pose2d(34, -50, pi/2), pi/2)
-//                .lineToY(-61.5, new TranslationalVelConstraint(30))
-
-
-//                .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/HangingAuto2.java
@@ -0,0 +1,147 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.roadrunner.PinpointDrive;
+import org.firstinspires.ftc.teamcode.robot.HangClaw;
+import org.firstinspires.ftc.teamcode.robot.IntakeSlide;
+import org.firstinspires.ftc.teamcode.robot.OuttakeSlide;
+
+@Autonomous(name="Hang2", group = "Auto")
+public final class HangingAuto2 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        final double pi = Math.PI;
+        Pose2d beginPose = new Pose2d(11, -62, Math.toRadians(-90));
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        OuttakeSlide outtakeSlide = new OuttakeSlide(hardwareMap);
+        HangClaw hangClaw = new HangClaw(hardwareMap);
+        IntakeSlide inSlide = new IntakeSlide(hardwareMap);
+
+        waitForStart();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        hangClaw.closeClaw(),
+                        inSlide.closeSlide(),
+                        // go to submersible
+                        new ParallelAction(
+                                drive.actionBuilder(beginPose)
+                                        .splineToConstantHeading(new Vector2d(11,-32.5), -pi/2)
+                                        .build()
+
+                        ),
+                        //hang the first specimen
+                        outtakeSlide.hang(),
+                        hangClaw.openClaw(),
+                        // go back a little bit
+                        drive.actionBuilder(new Pose2d(11, -32.5, Math.toRadians(-90)))
+                                .strafeTo(new Vector2d(11,-33))
+                                .build(),
+                        new ParallelAction(
+                                // Close the slide
+                                new SequentialAction(
+                                        // close the slide
+                                        outtakeSlide.spinDown(),
+                                        outtakeSlide.powerDown()),
+                                drive.actionBuilder(new Pose2d(11, -33, Math.toRadians(-90)))
+                                        .splineToConstantHeading(new Vector2d(11,-33),0 )
+                                        //Drive to First Block
+                                        .splineToSplineHeading(new Pose2d(32, -15, pi/2), pi/2)
+                                        .splineToConstantHeading(new Vector2d(42, -12), -pi/2)
+//                                // Push Block 1
+                                        .splineToConstantHeading(new Vector2d(45, -50), pi/2) // 54.5
+//                                // Go back, Spline Turn, and go to block 2
+//                                        .lineToY(-12)
+                                        .splineToConstantHeading(new Vector2d(45, -12), pi/2)
+                                        .splineToConstantHeading(new Vector2d(52, -12), -pi/2)
+//                                // Push Block 2
+                                        .splineToConstantHeading(new Vector2d(50, -50), pi/2) // 54.5
+//                                // Go back, and go pick up Block 1
+                                        .splineToConstantHeading(new Vector2d(35, -52), -pi/2)
+                                        .splineToConstantHeading(new Vector2d(35, -60), -pi/2, new TranslationalVelConstraint(30))
+                                        .build()
+                        ),
+                        hangClaw.closeClaw(),
+//                        new SleepAction(0.2),
+//                        new SleepAction(1),
+                        // go to submersible to hang specimen 2
+                        new SleepAction(0.3),
+                        new ParallelAction(
+                                outtakeSlide.spinUp(),
+                                drive.actionBuilder(new Pose2d(35,-60,Math.toRadians(-90)))
+                                        .strafeTo(new Vector2d(35,-53))
+                                        .splineToLinearHeading(new Pose2d(9, -31.5, -pi/2), pi/2)
+//                                        .strafeTo(new Vector2d(6, -29), new TranslationalVelConstraint(40))
+                                        .build()
+                        ),
+//                        new SleepAction(0.3),
+                        // hang speciment 2
+                        outtakeSlide.hang(),
+                        hangClaw.openClaw(),
+                        // go back to pick up specimen 3
+                        new ParallelAction(
+                                outtakeSlide.spinDown(),
+                                drive.actionBuilder(new Pose2d(9,-31.5,Math.toRadians(-90)))
+                                        .splineToLinearHeading(new Pose2d(32, -55, pi/2), 180)
+                                        .strafeTo(new Vector2d(32,-60), new TranslationalVelConstraint(30))
+                                        .build()
+                        ),
+//                        new SleepAction(0.5),
+                        //pick up specimen 3
+                        hangClaw.closeClaw(),
+                        new SleepAction(0.3),
+                        new ParallelAction(
+                                outtakeSlide.spinUp(),
+                                // go to submersible to hang specimen 3
+                                drive.actionBuilder(new Pose2d(32,-60,Math.toRadians(-90)))
+                                        .strafeTo(new Vector2d(32,-53))
+                                        .splineToLinearHeading(new Pose2d(8, -31, -pi/2), pi/2)
+                                        .build()
+                        ),
+//                        new SleepAction(0.3),
+                        // hang specimen 3
+                        outtakeSlide.hang(),
+                        hangClaw.openClaw(),
+//                        new SleepAction(0.5),
+                        // go for speciment 4
+                        new ParallelAction(
+                                outtakeSlide.spinDown(),
+                                drive.actionBuilder(new Pose2d(8,-31,Math.toRadians(-90)))
+                                        .splineToLinearHeading(new Pose2d(32, -55, pi/2), 180)
+                                        .strafeTo(new Vector2d(32,-60), new TranslationalVelConstraint(30))
+                                        .build()
+                        ),
+                        hangClaw.closeClaw(),
+                        new SleepAction(0.3),
+                        new ParallelAction(
+                                outtakeSlide.spinUp(),
+                                // go to submersible to hang specimen 4
+                                drive.actionBuilder(new Pose2d(32,-60,Math.toRadians(-90)))
+                                        .strafeTo(new Vector2d(32,-53))
+                                        .splineToLinearHeading(new Pose2d(6, -31, -pi/2), pi/2)
+                                        .build()
+                        ),
+                        // hang specimen 4
+                        outtakeSlide.hang(),
+                        hangClaw.openClaw(),
+                        // go for parking
+                        new ParallelAction(
+                                outtakeSlide.spinDown(),
+                                drive.actionBuilder(new Pose2d(6,-31,Math.toRadians(-90)))
+//                                        .splineToLinearHeading(new Pose2d(32, -15, pi/2), pi/2)
+                                        .splineToLinearHeading(new Pose2d(32, -57, pi/2), 180)
+//                                        .strafeTo(new Vector2d(32,-60), new TranslationalVelConstraint(30))
+                                        .build()
+                        )
+                )
+        );
+    }
+}
